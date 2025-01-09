bdf4f81a90edea5b00f2d3044b53065336381cf7
Samrath Singh <148793870+Samrath18@users.noreply.github.com>
2025-01-09 15:23:08 -0500

    Add files via upload

---
+++ ProductDB.java
@@ -0,0 +1,32 @@
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class ProductDB {
+
+
+    public static int add(Product product) {
+        var sql = "INSERT INTO products(name, price) "
+                + "VALUES(?,?)";
+
+        try (var conn =  DB.connect();
+             var pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            // bind the values
+            pstmt.setString(1, product.getName());
+            pstmt.setDouble(2, product.getPrice());
+
+            // execute the INSERT statement and get the inserted id
+            int insertedRow = pstmt.executeUpdate();
+            if (insertedRow > 0) {
+                var rs = pstmt.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+}
